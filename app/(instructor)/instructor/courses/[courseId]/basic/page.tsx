7a74d486950c8a70f1c8869cf62b5329e3546b79
anvo1405 <vohoaian62@gmail.com>
2024-07-10 22:57:09 +0700

    Edit Courses

--- app/(instructor)/instructor/courses/[courseId]/basic/page.tsx
+++ app/(instructor)/instructor/courses/[courseId]/basic/page.tsx
@@ -1,6 +1,52 @@
-const CourseBasics = () =>{
-    return(
-        <div>CourseBasics</div>
+import EditCoursesForm from "@/components/courses/EditCoursesForm"
+import { db } from "@/lib/db"
+import { auth } from "@clerk/nextjs/server"
+import { redirect } from "next/navigation"
+
+const CourseBasics = async ({ params }: { params: { courseId: string } }) => {
+    const { userId } = auth()
+
+    if (!userId) {
+        return redirect('/sign-in')
+    }
+
+    const course = await db.course.findUnique({
+        where: {
+            id: params.courseId,
+            instructorId: userId,
+        }
+    })
+
+    if (!course) {
+        return redirect('/instructor/courses')
+    }
+
+    const categories = await db.category.findMany({
+        orderBy: {
+            name: "asc"
+        },
+        include: {
+            subCategories: true
+        }
+    })
+
+    const levels = await db.level.findMany()
+
+    return (
+        <div className="px-10">
+            <EditCoursesForm course={course} categories={categories.map((category) => ({
+                label: category.name,
+                value: category.id,
+                subCategories: category.subCategories.map((subcategory) => ({
+                    label: subcategory.name,
+                    value: subcategory.id,
+                }))
+            }))}
+                levels={levels.map((level) => ({
+                    label: level.name,
+                    value: level.id,
+                }))} />
+        </div>
     )
 }
 
