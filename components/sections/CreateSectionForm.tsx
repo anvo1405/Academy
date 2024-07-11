29e5652c19702632bd9d0685fa05104fc79f8769
anvo1405 <vohoaian62@gmail.com>
2024-07-12 00:18:20 +0700

    Reorder Section

--- components/sections/CreateSectionForm.tsx
+++ components/sections/CreateSectionForm.tsx
@@ -1,37 +1,46 @@
-"use client"
-import { Course } from "@prisma/client";
+"use client";
+
+import { Course, Section } from "@prisma/client";
 import Link from "next/link";
 import { usePathname, useRouter } from "next/navigation";
-import { z } from "zod"
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
 
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
     Form,
     FormControl,
-    FormDescription,
     FormField,
     FormItem,
     FormLabel,
     FormMessage,
-} from "@/components/ui/form"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
 import toast from "react-hot-toast";
 import axios from "axios";
+import SectionList from "@/components/sections/SectionList";
+import { Loader2 } from "lucide-react";
 
 const formSchema = z.object({
     title: z.string().min(2, {
         message: "Title is required and must be at least 2 characters long",
     }),
-})
+});
 
-const CreateSectionForm = ({ course }: { course: Course }) => {
+const CreateSectionForm = ({
+    course,
+}: {
+    course: Course & { sections: Section[] };
+}) => {
     const pathname = usePathname();
     const router = useRouter();
+
     const routes = [
-        { label: "Basic Information", path: `/instructor/courses/${course.id}/basic` },
+        {
+            label: "Basic Information",
+            path: `/instructor/courses/${course.id}/basic`,
+        },
         { label: "Curriculum", path: `/instructor/courses/${course.id}/sections` },
     ];
 
@@ -41,30 +50,61 @@ const CreateSectionForm = ({ course }: { course: Course }) => {
         defaultValues: {
             title: "",
         },
-    })
+    });
+
+    const { isValid, isSubmitting } = form.formState;
 
     // 2. Define a submit handler.
-    const onSubmit = async (values: z.infer<typeof formSchema>) =>{
-        try{
-            const response = await axios.post(`/api/courses/${course.id}/sections`, values);
-            router.push(`/instructor/courses/${course.id}/sections/${response.data.id}`)
+    const onSubmit = async (values: z.infer<typeof formSchema>) => {
+        try {
+            const response = await axios.post(
+                `/api/courses/${course.id}/sections`,
+                values
+            );
+            router.push(
+                `/instructor/courses/${course.id}/sections/${response.data.id}`
+            );
             toast.success("New Section created!");
-        } catch(err){
+        } catch (err) {
             toast.error("Something went wrong!");
-            console.log("Failed to create a new section", err)
+            console.log("Failed to create a new section", err);
         }
-    }
+    };
+
+    const onReorder = async (updateData: { id: string; position: number }[]) => {
+        try {
+            await axios.put(`/api/courses/${course.id}/sections/reorder`, {
+                list: updateData,
+            });
+            toast.success("Sections reordered successfully");
+        } catch (err) {
+            console.log("Failed to reorder sections", err);
+            toast.error("Something went wrong!");
+        }
+    };
 
     return (
         <div className="px-10 py-6">
             <div className="flex gap-5">
                 {routes.map((route) => (
-                    <Link key={route.label} href={route.path} className="flex gap-4">
-                        <Button variant={pathname === route.path ? "default" : "outline"}>{route.label}</Button>
+                    <Link key={route.path} href={route.path}>
+                        <Button variant={pathname === route.path ? "default" : "outline"}>
+                            {route.label}
+                        </Button>
                     </Link>
                 ))}
             </div>
+
+            <SectionList
+                items={course.sections || []}
+                onReorder={onReorder}
+                onEdit={(id) =>
+                    router.push(`/instructor/courses/${course.id}/sections/${id}`)
+                }
+            />
+
             <h1 className="text-xl font-bold mt-5">Add New Section</h1>
+
             <Form {...form}>
                 <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 mt-5">
                     <FormField
@@ -80,16 +120,25 @@ const CreateSectionForm = ({ course }: { course: Course }) => {
                             </FormItem>
                         )}
                     />
+
                     <div className="flex gap-5">
                         <Link href={`/instructor/courses/${course.id}/basic`}>
-                        <Button variant="outline" type="button">Cancel</Button>
+                            <Button variant="outline" type="button">
+                                Cancel
+                            </Button>
                         </Link>
-                    <Button type="submit">Submit</Button>
+                        <Button type="submit" disabled={!isValid || isSubmitting}>
+                            {isSubmitting ? (
+                                <Loader2 className="h-4 w-4 animate-spin" />
+                            ) : (
+                                "Create"
+                            )}
+                        </Button>
                     </div>
                 </form>
             </Form>
         </div>
-    )
-}
+    );
+};
 
-export default CreateSectionForm
+export default CreateSectionForm;
